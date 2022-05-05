6ce8fbd7c946897784432feb35bb6ea5c5724529
Serena Lew <36708701+minibaguettes@users.noreply.github.com>
2022-05-04 21:50:08 -0700

    section 68: home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home' # home action must be in controllers/application_controller.rb
+  get 'about', to: 'pages#about' # send to controllername#action pages#about
 end
